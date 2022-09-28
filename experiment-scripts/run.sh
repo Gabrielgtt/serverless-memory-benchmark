28ee1aeb3005b2a7dcf99f56d0b23543d089eb29
moranguinho <gabriel.tavares@splab.ufcg.edu.br>
2022-09-28 10:05:27 -0300

    Kill container after experiment

--- experiment-scripts/run.sh
+++ experiment-scripts/run.sh
@@ -14,19 +14,21 @@ do
     esac
 done
 
-BENCHID=$(sudo docker run -it -d $REPOSITORY:$TAG)
-CONTAINER_WD=$(sudo docker inspect --format='{{.Config.WorkingDir}}' $BENCHID)
+CONTAINERID=$(sudo docker run -it -d $REPOSITORY:$TAG)
+CONTAINER_WD=$(sudo docker inspect --format='{{.Config.WorkingDir}}' $CONTAINERID)
+
+echo $CONTAINERID
 
 execute_benchmark() {
     exp_tag=$1
     gc_log_fp="${CONTAINER_WD}/${exp_tag}.log"
 
-    sudo docker cp $INPUT_FILE ${BENCHID}:${CONTAINER_WD}/input.json
+    sudo docker cp $INPUT_FILE ${CONTAINERID}:${CONTAINER_WD}/input.json
 
-    sudo docker exec $BENCHID bash -c \
+    sudo docker exec $CONTAINERID bash -c \
         "rm -rf ${gc_log_fp}*"
 
-    sudo docker exec $BENCHID bash -c \
+    sudo docker exec $CONTAINERID bash -c \
         "java -XX:+Use${GC}GC -Xlog:gc:file=${gc_log_fp}:uptime,tags,level:filecount=1,filesize=6g -Xms${HEAP_SIZE} -Xmx${HEAP_SIZE} -cp Orchestrator.jar Main -b $BENCHMARK -i input.json -n $WARMUP"
 }
 
@@ -36,8 +38,13 @@ collect_results() {
 
     mkdir ./$exp_tag/
 
-    sudo docker cp ${BENCHID}:${CONTAINER_WD}/results ./${exp_tag}/results
-    sudo docker cp ${BENCHID}:${gc_log_fp} ./${exp_tag}/gc.log
+    sudo docker cp ${CONTAINERID}:${CONTAINER_WD}/results ./${exp_tag}/results
+    sudo docker cp ${CONTAINERID}:${gc_log_fp} ./${exp_tag}/gc.log
+}
+
+kill_container() {
+    sudo docker kill $CONTAINERID
+    sudo docker rm $CONTAINERID
 }
 
 echo_line() {
@@ -57,8 +64,12 @@ do
 
     echo_line
     echo "Running $exp_tag" 
-
     execute_benchmark $exp_tag
+
+    echo "Collecting results"
     collect_results $exp_tag
+
+    echo "Killing container"
+    kill_container
 done
 
