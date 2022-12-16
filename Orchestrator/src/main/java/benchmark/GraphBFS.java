2883bbe5814097b6765a8ddaff25396b316d7e4e
moranguinho <gabriel.tavares@splab.ufcg.edu.br>
2022-12-16 16:18:33 -0300

    Add allocated memory per request

--- Orchestrator/src/main/java/benchmark/GraphBFS.java
+++ Orchestrator/src/main/java/benchmark/GraphBFS.java
@@ -1,6 +1,7 @@
 package benchmark;
 
 import java.io.FileWriter;
+import java.lang.Runtime;
 
 public class GraphBFS implements Benchmark {
 
@@ -11,17 +12,23 @@ public class GraphBFS implements Benchmark {
     }
 
     public void execute(int n, int batch_size, FileWriter file, String[] args) throws Exception {
+        Runtime rt = Runtime.getRuntime();
         for (int j = 0; j < n; j++) {
             System.gc();
 
             for (int i = 0; i < batch_size; i++) {
                 System.out.println("Execution " + i);
+                long free_before = rt.freeMemory();
                 long start = System.currentTimeMillis();
+
                 graphbfs.Main.main(args);
+
+                long free_after = rt.freeMemory();
+                long allocated = (free_before - free_after);
                 long end = System.currentTimeMillis();
                 long duration = end - start;
 
-                file.write(String.format("%s,%s,%s,%s,%s\n", this.getName(), i, start, end, duration));
+                file.write(String.format("%s,%s,%s,%s,%s,%s\n", this.getName(), i, start, end, duration, allocated));
             }
         }
     }
--- Orchestrator/src/main/java/benchmark/Sleep.java
+++ Orchestrator/src/main/java/benchmark/Sleep.java
@@ -1,8 +1,10 @@
 package benchmark;
 
 import java.io.FileWriter;
+import java.lang.Runtime;
 
 public class Sleep implements Benchmark {
+
     private static final String name = "sleep";
 
     public String getName() {
@@ -10,17 +12,23 @@ public class Sleep implements Benchmark {
     }
 
     public void execute(int n, int batch_size, FileWriter file, String[] args) throws Exception {
+        Runtime rt = Runtime.getRuntime();
         for (int j = 0; j < n; j++) {
             System.gc();
 
             for (int i = 0; i < batch_size; i++) {
                 System.out.println("Execution " + i);
+                long free_before = rt.freeMemory();
                 long start = System.currentTimeMillis();
+
                 sleep.Main.main(args);
+
+                long free_after = rt.freeMemory();
+                long allocated = (free_before - free_after);
                 long end = System.currentTimeMillis();
                 long duration = end - start;
 
-                file.write(String.format("%s,%s,%s,%s,%s\n", this.getName(), i, start, end, duration));
+                file.write(String.format("%s,%s,%s,%s,%s,%s\n", this.getName(), i, start, end, duration, allocated));
             }
         }
     }
