b6e3ccfbc32937acd45effe71e116e2a63a6102f
moranguinho <gabriel.tavares@splab.ufcg.edu.br>
2023-01-30 20:45:12 -0300

    Add -f flag in run-all.sh

--- experiment-scripts/run-all.sh
+++ experiment-scripts/run-all.sh
@@ -12,14 +12,14 @@ N=1
 GCS=("G1")
 BENCHMARKS=("video-processing")
 INPUT_FILES=("input-video-processing.json")
-BATCH_SIZE=10
+BATCH_SIZE=100
 CPUS=("2.0")
 FLAGS=""
 
 for heap_index in ${!HEAP_SIZES[@]}; do
     for gc in ${GCS[@]}; do
         for benchmark_index in ${!BENCHMARKS[@]}; do
-            sudo bash run.sh -r $REPOSITORY -t $TAG -h ${HEAP_SIZES[$heap_index]} -w $WARMUP -n $N -g $gc -b ${BENCHMARKS[$benchmark_index]} -i ${INPUT_WD}${INPUT_FILES[$benchmark_index]} -s $BATCH_SIZE -c ${CPUS[$heap_index]} > experiment-$gc-${BENCHMARKS[$benchmark_index]}-${HEAP_SIZES[$heap_index]}.out 2> experiment-$gc-${BENCHMARKS[$benchmark_index]}-${HEAP_SIZES[$heap_index]}.err
+            sudo bash run.sh -f $FLAGS -r $REPOSITORY -t $TAG -h ${HEAP_SIZES[$heap_index]} -w $WARMUP -n $N -g $gc -b ${BENCHMARKS[$benchmark_index]} -i ${INPUT_WD}${INPUT_FILES[$benchmark_index]} -s $BATCH_SIZE -c ${CPUS[$heap_index]} > experiment-$gc-${BENCHMARKS[$benchmark_index]}-${HEAP_SIZES[$heap_index]}.out 2> experiment-$gc-${BENCHMARKS[$benchmark_index]}-${HEAP_SIZES[$heap_index]}.err
         done
     done
 done
