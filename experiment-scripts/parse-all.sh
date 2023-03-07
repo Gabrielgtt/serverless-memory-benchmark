e60eee0ac2693fc21f9f642306cc935d9a9fff4c
moranguinho <gabriel.tavares@splab.ufcg.edu.br>
2023-03-07 09:49:20 -0300

    Add env vars to parse-all script

--- experiment-scripts/parse-all.sh
+++ experiment-scripts/parse-all.sh
@@ -1,19 +1,6 @@
 #!/bin/bash/
 
-# set -x
-
-# bash parse-data.sh -r 10 -g Shenandoah -p /home/moranguinho/mestrado/serverless-memory-benchmark/experiment-analysis/results/exp-17-10/exp-Shenandoah-dynamic-html-4g-
-
-INPUT_WD="/home/moranguinho/mestrado/serverless-memory-benchmark/experiment-analysis/results/exp-23-01/"
-
-# HEAP_SIZES=("256m" "512m" "1g" "2g" "4g")
-HEAP_SIZES=("20g2c" "20g1c")
-N=20
-# GCS=("G1" "Serial" "Shenandoah")
-# PARSERS=("G1" "G1" "Shenandoah")
-GCS=("Epsilon")
-PARSERS=("Epsilon")
-BENCHMARKS=("dynamic-html")
+source ./vars.env
 
 for heap in ${HEAP_SIZES[@]}; do
     for gc_index in ${!GCS[@]}; do
