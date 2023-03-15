2db0503a595b0404df35e8bc0b8557e8dd6eaac1
Gabrielgtt <gabrielgtt@hotmail.com>
2023-03-15 11:13:27 -0300

    Remove auxiliar script

--- experiment-scripts/auxiliar.sh
+++
@@ -1,7 +0,0 @@
-# Para copiar os resultados de uma VM para sua máquina local
-seq 1 10 | xargs -I XXX scp -r -i ~/.ssh/doritos.pem ubuntu@10.11.19.173:"/home/ubuntu/serverless-memory-benchmark/experiment-scripts/exp-Epsilon-dynamic-html-XXX/" ./
-
-seq 1 10 | xargs -I XXX scp -r -i ~/.ssh/doritos.pem ubuntu@10.11.19.173:"/home/ubuntu/serverless-memory-benchmark/experiment-scripts/exp-Epsilon-dynamic-html-XXX/" ./
-
-
-
