720658e123dba0015b726f1de8bf5753ff1771fd
Gabrielgtt <gabrielgtt@hotmail.com>
2023-03-17 16:11:55 -0300

    Final additions of README and R script

--- experiment-scripts/run.sh
+++ experiment-scripts/run.sh
@@ -2,7 +2,9 @@
 
 set -x
 
-while getopts r:t:h:w:n:g:b:i:s:c: flag
+EXP_DATE=$(date +'%d-%m')
+
+while getopts r:t:h:w:n:g:b:i:s:c:d: flag
 do
         case "${flag}" in
                 r) REPOSITORY=${OPTARG};;
@@ -15,10 +17,10 @@ do
                 i) INPUT_FILE=${OPTARG};;
                 s) BATCH_SIZE=${OPTARG};;
                 c) CPU=${OPTARG};;
+                d) EXP_DATE=${OPTARG};;
         esac
 done
 
-DATE_TODAY=$(date +'%d-%m')
 JVM_FLAGS=""
 
 if [ $GC == "Epsilon" ] 
@@ -30,7 +32,7 @@ read -r -d '' EXP_DESCRITION << EOM
     Heap size = $HEAP_SIZE\nWarmup runs = $WARMUP\nNumber of repetitions = $N\nGc = $GC\nBenchmark (function name) = $BENCHMARK\nInput file = $INPUT_FILE\nBatch size = $BATCH_SIZE\nNumber of CPU cores = $CPU\nJVM Flags = $JVM_FLAGS
 EOM
 
-bash discord-bot.sh -m "!! EXPERIMENT STARTING !!" -t "exp-$DATE_TODAY: $(date)" -d "$EXP_DESCRITION" -s ok
+bash discord-bot.sh -m "!! EXPERIMENT STARTING !!" -t "exp-$EXP_DATE: $(date)" -d "$EXP_DESCRITION" -s ok
 
 echo "CAREFUL! We are using hard-coded 21g docker container memory!!"
 CONTAINERID=$(sudo docker run -it -d --cpus="${CPU}" --memory="21g" $REPOSITORY:$TAG)
@@ -38,10 +40,10 @@ CONTAINERID=$(sudo docker run -it -d --cpus="${CPU}" --memory="21g" $REPOSITORY:
 
 if [ $? -eq 0 ]; then
         echo "OK"
-        bash discord-bot.sh -m "Container created" -t "exp-$DATE_TODAY" -d "Começamos bem!" -s ok
+        bash discord-bot.sh -m "Container created" -t "exp-$EXP_DATE" -d "Começamos bem!" -s ok
 else
         ERROR_MSG="Error while running docker run. See .err file"
-        bash discord-bot.sh -m "Container failed" -t "exp-$DATE_TODAY" -d "$ERROR_MSG" -s error
+        bash discord-bot.sh -m "Container failed" -t "exp-$EXP_DATE" -d "$ERROR_MSG" -s error
         echo $ERROR_MSG
         exit $?
 fi
@@ -67,12 +69,12 @@ collect_results() {
         exp_tag=$1
         gc_log_fp="${CONTAINER_WD}/${exp_tag}.log"
 
-        mkdir ./$exp_tag/
+        mkdir ./output/$exp_tag/
 
-        sudo docker cp ${CONTAINERID}:${CONTAINER_WD}/results ./${exp_tag}/results
-        sudo docker cp ${CONTAINERID}:${gc_log_fp} ./${exp_tag}/gc.log
-        sudo docker cp ${CONTAINERID}:${CONTAINER_WD}/orchestrator.out ./${exp_tag}/orchestrator.out
-        sudo docker cp ${CONTAINERID}:${CONTAINER_WD}/orchestrator.err ./${exp_tag}/orchestrator.err
+        sudo docker cp ${CONTAINERID}:${CONTAINER_WD}/results ./output/${exp_tag}/results
+        sudo docker cp ${CONTAINERID}:${gc_log_fp} ./output/${exp_tag}/gc.log
+        sudo docker cp ${CONTAINERID}:${CONTAINER_WD}/orchestrator.out ./output/${exp_tag}/orchestrator.out
+        sudo docker cp ${CONTAINERID}:${CONTAINER_WD}/orchestrator.err ./output/${exp_tag}/orchestrator.err
 }
 
 kill_container() {
@@ -83,11 +85,10 @@ kill_container() {
 
 compress_output() {
     trunc_cpu="${CPU/\.*/}c"
-    exp_tag=exp-${GC}-${BENCHMARK}-${HEAP_SIZE}${trunc_cpu}
-    zip_fp=exp-$DATE_TODAY.zip
+    exp_tag=output/exp-${GC}-${BENCHMARK}-${HEAP_SIZE}${trunc_cpu}
+    zip_fp=output/exp-$EXP_DATE.zip
     zip -r $zip_fp ${exp_tag}*
 }
-
 
 
 for exp in $(seq 1 $N);
@@ -95,16 +96,17 @@ do
         trunc_cpu="${CPU/\.*/}c"
         exp_tag=exp-${GC}-${BENCHMARK}-${HEAP_SIZE}${trunc_cpu}-${exp}
 
-        if [[ -d $exp_tag ]]
+        exp_dir=output/$exp_tag
+        if [[ -d $exp_dir ]]
         then
-                ERROR_MSG="Directory $exp_tag already exists"
+                ERROR_MSG="Directory $exp_dir already exists"
                 echo $ERROR_MSG
-                bash discord-bot.sh -m "Experiment stopped:" -t "exp-$DATE_TODAY" -d "$ERROR_MSG" -s error
+                bash discord-bot.sh -m "Experiment stopped:" -t "exp-$EXP_DATE" -d "$ERROR_MSG" -s error
                 kill_container
                 exit 1
         fi
 
-        echo "-----------"
+        echo "-----------------------------"
 
         execute_benchmark $exp_tag
 
@@ -113,7 +115,7 @@ done
 
 compress_output
 
-bash discord-bot.sh -m "Experiment finished!" -t "exp-$DATE_TODAY" -d "Tudo certo pelo visto!" -s ok
+bash discord-bot.sh -m "Experiment finished!" -t "exp-$EXP_DATE" -d "Tudo certo pelo visto!" -s ok
 
 kill_container
 
