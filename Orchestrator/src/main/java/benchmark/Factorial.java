8072d5647b346fff0eef4adcf85ae59ff8d5c04d
wendell.nascimento <wendell.nascimento@ccc.ufcg.edu.br>
2023-05-02 17:05:57 -0300

    ajustando codigo para coletar uso da cpu e tempo de uso da cpu

--- Orchestrator/src/main/java/benchmark/Factorial.java
+++ Orchestrator/src/main/java/benchmark/Factorial.java
@@ -2,6 +2,8 @@ package benchmark;
 
 import java.io.FileWriter;
 import java.lang.Runtime;
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
 
 public class Factorial implements Benchmark {
 
@@ -13,6 +15,7 @@ public class Factorial implements Benchmark {
 
     public void execute(int n, int batch_size, FileWriter file, String[] args) throws Exception {
         Runtime rt = Runtime.getRuntime();
+        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
         for (int j = 0; j < n; j++) {
             System.gc();
 
@@ -20,15 +23,18 @@ public class Factorial implements Benchmark {
                 System.out.println("Execution " + i);
                 long free_before = rt.freeMemory();
                 long start = System.nanoTime();
-
+                long startTimeCpu = osBean.getProcessCpuTime();
                 factorial.Main.main(args);
-
+                long finishTimeCpu = osBean.getProcessCpuTime();
                 long free_after = rt.freeMemory();
                 long allocated = (free_before - free_after);
                 long end = System.nanoTime();
                 long duration = end - start;
+                double cpuUsedProcess = osBean.getProcessCpuLoad();
+                long cpuTimeProcess = finishTimeCpu - startTimeCpu;
 
-                file.write(String.format("%s,%s,%s,%s,%s,%s\n", this.getName(), i, start, end, duration, allocated));
+                file.write(String.format("%s,%s,%s,%s,%s,%s,%s,%s\n", this.getName(), i, start, end, duration, allocated, cpuTimeProcess, cpuUsedProcess));
+
             }
         }
     }
--- Orchestrator/src/main/java/benchmark/VideoProcessing.java
+++ Orchestrator/src/main/java/benchmark/VideoProcessing.java
@@ -4,6 +4,8 @@ import java.io.FileWriter;
 import java.nio.file.NotDirectoryException;
 import java.io.File;
 import java.lang.Runtime;
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
 
 public class VideoProcessing implements Benchmark {
 
@@ -15,6 +17,7 @@ public class VideoProcessing implements Benchmark {
 
     public void execute(int n, int batch_size, FileWriter file, String[] args) throws Exception {
         Runtime rt = Runtime.getRuntime();
+        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
 
         File dir = new File(args[1]);
 
@@ -27,21 +30,25 @@ public class VideoProcessing implements Benchmark {
 
         for (int j = 0; j < n; j++) {
             System.gc();
-
             for (int i = 0; i < batch_size; i++) {
                 System.out.println("Execution " + i);
                 args[1] = files[i % nfiles];
                 long free_before = rt.freeMemory();
                 long start = System.nanoTime();
+                long startTimeCpu = osBean.getProcessCpuTime();
 
                 videoprocessing.Main.main(args);
 
+                long finishTimeCpu = osBean.getProcessCpuTime();
                 long free_after = rt.freeMemory();
                 long allocated = (free_before - free_after);
                 long end = System.nanoTime();
                 long duration = end - start;
+                double cpuUsedProcess = osBean.getProcessCpuLoad();
+                long cpuTimeProcess = finishTimeCpu - startTimeCpu;
 
-                file.write(String.format("%s,%s,%s,%s,%s,%s\n", this.getName(), i, start, end, duration, allocated));
+                file.write(String.format("%s,%s,%s,%s,%s,%s,%s,%s\n", this.getName(), i, start, end, duration, allocated, cpuTimeProcess, cpuUsedProcess));
+
             }
         }
     }
